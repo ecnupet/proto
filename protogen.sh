33823187065026ed9ba4dd15f436fd8c2fbd467f
shanchao <chao.shan01@liulishuo.com>
2021-03-09 20:05:53 +0800

    ttttttttt

--- protogen.sh
+++ protogen.sh
@@ -7,6 +7,7 @@ git config --global user.name "tonyshanc"
 git config --global user.email "845700113@qq.com"
 git config --global push.default simple
 git remote set-url origin https://tonyshanc:${CI_PUSH_TOKEN}@github.com:ecnupet/proto.git
+git fetch origin main
 latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 git_head=`git rev-parse --short HEAD`
 
