1cee06f0924e5b431bda2bd0f6760f27158ee46f
shanchao <chao.shan01@liulishuo.com>
2021-03-10 01:47:09 +0800

    tttt

--- protogen.sh
+++ protogen.sh
@@ -4,12 +4,8 @@ set -e
 echo -e "\033[0;32mgit config\033[0m"
 # 78125d8696da6829459c1e9074731b37709b486a
 export CI_PUSH_TOKEN=78125d8696da6829459c1e9074731b37709b486a
-git config --global --add url."git@github.com:".insteadOf "https://github.com"
 git config --global user.name "TonyShanc"
-git config --global user.password ${CI_PUSH_TOKEN}
 git config --global user.email "845700113@qq.com"
-git config --global push.default simple
-git remote set-url origin https://github.com/ecnupet/proto.git
 git clone https://github.com/ecnupet/proto.git
 cd proto
 git fetch origin main
